f03abe035813f7934596850df189a19b391a00ce
mdCarron <59117899+mdCarron@users.noreply.github.com>
2020-09-11 18:41:15 -0300

    Completa 11. Documentando la App

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# ctc-clima
- CTC - Desarrollo FrontEnd con React
+# [Consultar Clima App]()
+
+## Deploy 🔻
+
+[Consultar Clima App]()
+
+## ⭐ Acerca del Proyecto
+
+Proyecto de aprendizaje realizado en el curso **Desarrollo FrontEnd con React**, gracias a la iniciativa de capacitación en tecnologías digitales por el Cluster Tecnológico de Catamarca (CTC).
+
+## 🔍 Objetivos
+
+- Profundiza en los elementos básicos de React
+- PropTypes
+- Fetch APIs
+
+## ✅ Tecnologías
+
+- Javascript
+- React
--- src/App.js
+++ src/App.js
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { Fragment, useState, useEffect } from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import Clima from "./components/Clima";
+import Error from "./components/Error";
 
 function App() {
-  return <h1>Clima desde API</h1>;
+  const [busqueda, guardarBusqueda] = useState({
+    ciudad: "",
+    pais: "",
+  });
+  const [consulta, guardarConsulta] = useState(false);
+  const [resultado, guardarResultado] = useState({});
+  const [error, guardarError] = useState(false);
+
+  const { ciudad, pais } = busqueda;
+
+  useEffect(() => {
+    if (consulta) {
+      const consultarApi = async () => {
+        const API_KEY = "cc9c8f3792a516cd56ffae95c10a98b6";
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${API_KEY}`;
+        const fetchData = await fetch(url);
+        const responseData = await fetchData.json();
+        guardarResultado(responseData);
+        guardarConsulta(false);
+        if (responseData.cod === "404") {
+          guardarError(true);
+        } else {
+          guardarError(false);
+        }
+      };
+      consultarApi();
+    }
+  }, [consulta, ciudad, pais]);
+
+  return (
+    <Fragment>
+      <Header titulo="CLIMA React App" />
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario
+                busqueda={busqueda}
+                guardarBusqueda={guardarBusqueda}
+                guardarConsulta={guardarConsulta}
+              />
+            </div>
+            <div className="col m6 s12">
+              {error ? (
+                <Error mensaje="No se encontraron resultados" />
+              ) : (
+                <Clima resultado={resultado} />
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
